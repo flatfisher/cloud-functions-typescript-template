6a3e7769740e5e8b810e8211e9541853e4d62f5b
flatfisher <shimano.entou@gmail.com>
2018-10-14 11:40:18 +0900

    Remove semicolon

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { Request, Response } from 'express';
+import { Request, Response } from 'express'
 
 export function helloWorld(_: Request, res: Response) {
     try {
-        res.status(200);
-        res.send('Hello World');
+        res.status(200)
+        res.send('Hello World')
     } catch (err) {
-        res.status(500);
-        res.send(err);
+        res.status(500)
+        res.send(err)
     }
 }
--- test/test_helloworld.ts
+++ test/test_helloworld.ts
@@ -1,24 +1,24 @@
-import chai = require('chai');
-import chaiHttp = require('chai-http');
-import * as express from "express";
+import chai = require('chai')
+import chaiHttp = require('chai-http')
+import * as express from "express"
 
-import { helloWorld } from '../src/index';
+import { helloWorld } from '../src/index'
 
-const app = express();
+const app = express()
 app.get('/', helloWorld)
 
-chai.use(chaiHttp);
-const expect = chai.expect;
+chai.use(chaiHttp)
+const expect = chai.expect
 
 describe('Hello function', () => {
     it('Get 200 response', function (done) {
         chai.request(app)
             .get('/')
             .end((err, res) => {
-                expect(err).to.be.null;
-                expect(res.text).to.be.equal('Hello World');
-                expect(res).to.have.status(200);
-                done();
-            });
-    });
-});
+                expect(err).to.be.null
+                expect(res.text).to.be.equal('Hello World')
+                expect(res).to.have.status(200)
+                done()
+            })
+    })
+})
