a6db07af1a14d18e18b49feed054251b18cbd6c1
flatfisher <shimano.entou@gmail.com>
2019-11-20 23:36:43 +0900

    Update

--- cli.js
+++ cli.js
@@ -1,7 +1,42 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const path = require('path');
+
 function main() {
-    console.log("create-project");
+    if (process.argv.length > 2) {
+        copyFolderRecursiveSync("./", process.argv[2])
+    }
+}
+
+function copyFileSync(source, target) {
+    let targetFile = target;
+    if (fs.existsSync(target)) {
+        if (fs.lstatSync(target).isDirectory()) {
+            targetFile = path.join(target, path.basename(source));
+        }
+    }
+
+    fs.writeFileSync(targetFile, fs.readFileSync(source));
+}
+
+function copyFolderRecursiveSync(source, target) {
+    let files = [];
+    let targetFolder = path.join(target, path.basename(source));
+    if (!fs.existsSync(targetFolder)) {
+        fs.mkdirSync(targetFolder);
+    }
+    if (fs.lstatSync(source).isDirectory()) {
+        files = fs.readdirSync(source);
+        files.forEach(function(file) {
+            let curSource = path.join(source, file);
+            if (fs.lstatSync(curSource).isDirectory()) {
+                copyFolderRecursiveSync(curSource, targetFolder);
+            } else {
+                copyFileSync(curSource, targetFolder);
+            }
+        });
+    }
 }
 
 main();
