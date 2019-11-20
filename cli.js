5874fa4fcce5b84290532cdab20f81d848865b11
flatfisher <shimano.entou@gmail.com>
2019-11-20 23:41:06 +0900

    Update

--- cli.js
+++ cli.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 function main() {
     if (process.argv.length > 2) {
-        copyFolderRecursiveSync("./", process.argv[2])
+        copyFolderRecursiveSync("./node_modules/google-cloud-functions-typescript/", process.argv[2])
     }
 }
 
